1b83d7b0e4fa07376be7a709cbb889f6fb2f63ad
iAverages <31937542+iAverages@users.noreply.github.com>
2023-09-11 20:03:41 +0100

    Remove code from removed deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,13 @@
-import { type Session } from "next-auth";
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
-const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps }) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
     return (
         <ThemeProvider attribute="class">
-            {/* Leaving it here because im too lazy to readd it in the future, not needed right now */}
-            {/* <SessionProvider session={session}> */}
             <Component {...pageProps} />
-            {/* </SessionProvider> */}
         </ThemeProvider>
     );
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;
