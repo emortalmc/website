4e759a2d21a13276e705f52018c76c610e02e7d9
iAverages <31937542+iAverages@users.noreply.github.com>
2023-09-11 20:39:05 +0100

    Just messing around with some designs

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,10 +2,26 @@ import Link from "next/link";
 import { type HTMLAttributes, type ReactNode } from "react";
 import { Icons } from "~/components/icons";
 import { ThemeToggle } from "~/components/themeToggle";
-import { buttonVariants } from "~/components/ui/button";
+import { Button, buttonVariants } from "~/components/ui/button";
 import { cn } from "~/utils/ui";
 import { siteConfig } from "~/config/site";
 import { type VariantProps } from "class-variance-authority";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { MoonIcon, Settings, SunIcon, User } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     asChild?: boolean;
@@ -37,6 +53,8 @@ const NavLink = ({ children, href, newTab, className, size, ...props }: NavLinkP
 };
 
 export const Naviation = () => {
+    const { setTheme } = useTheme();
+
     return (
         <div
             className={
@@ -55,8 +73,198 @@ export const Naviation = () => {
                 <NavLink href={siteConfig.links.discord} newTab>
                     <Icons.discord className="h-4 w-4" />
                 </NavLink>
-                <ThemeToggle />
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        {/* <User
+                            className={cn(
+                                buttonVariants({
+                                    variant: "ghost",
+                                    size: "icon",
+                                })
+                                // "h-4 w-4"
+                            )}
+                        /> */}
+                        <div>
+                            <img src={"https://skins.danielraybone.com/v1/head/iAverage"} width={25} height={25} />
+                        </div>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuLabel>{"{USERNAME}"}</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <User className="mr-2 h-4 w-4" />
+                                <span>Profile</span>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Settings className="mr-2 h-4 w-4" />
+                                <span>Settings</span>
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem asChild>
+                                <DropdownMenuSub>
+                                    <DropdownMenuSubTrigger>
+                                        <SunIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                                        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+
+                                        <span className={"ml-6"}>Theme</span>
+                                    </DropdownMenuSubTrigger>
+                                    <DropdownMenuPortal>
+                                        <DropdownMenuSubContent>
+                                            <DropdownMenuItem onClick={() => setTheme("light")}>
+                                                <SunIcon className="mr-2 h-[1.2rem] w-[1.2rem]" />
+                                                <span>Light</span>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                                                <MoonIcon className="mr-2 h-[1.2rem] w-[1.2rem]" />
+                                                <span>Dark</span>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem onClick={() => setTheme("system")}>
+                                                {/* <PlusCircle className="mr-2 h-4 w-4" /> */}
+                                                <span>System</span>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuSubContent>
+                                    </DropdownMenuPortal>
+                                </DropdownMenuSub>
+
+                                {/* <ThemeToggle /> */}
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <span>Logout</span>
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     );
 };
+
+// import {
+//     Cloud,
+//     CreditCard,
+//     Github,
+//     Keyboard,
+//     LifeBuoy,
+//     LogOut,
+//     Mail,
+//     MessageSquare,
+//     Plus,
+//     PlusCircle,
+//     Settings,
+//     User,
+//     UserPlus,
+//     Users,
+//   } from "lucide-react"
+
+//   import { Button } from "@/components/ui/button"
+//   import {
+//     DropdownMenu,
+//     DropdownMenuContent,
+//     DropdownMenuGroup,
+//     DropdownMenuItem,
+//     DropdownMenuLabel,
+//     DropdownMenuPortal,
+//     DropdownMenuSeparator,
+//     DropdownMenuShortcut,
+//     DropdownMenuSub,
+//     DropdownMenuSubContent,
+//     DropdownMenuSubTrigger,
+//     DropdownMenuTrigger,
+//   } from "@/components/ui/dropdown-menu"
+
+//   export function DropdownMenuDemo() {
+//     return (
+//       <DropdownMenu>
+//         <DropdownMenuTrigger asChild>
+//           <Button variant="outline">Open</Button>
+//         </DropdownMenuTrigger>
+//         <DropdownMenuContent className="w-56">
+//           <DropdownMenuLabel>My Account</DropdownMenuLabel>
+//           <DropdownMenuSeparator />
+//           <DropdownMenuGroup>
+//             <DropdownMenuItem>
+//               <User className="mr-2 h-4 w-4" />
+//               <span>Profile</span>
+//               <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+//             </DropdownMenuItem>
+//             <DropdownMenuItem>
+//               <CreditCard className="mr-2 h-4 w-4" />
+//               <span>Billing</span>
+//               <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+//             </DropdownMenuItem>
+//             <DropdownMenuItem>
+//               <Settings className="mr-2 h-4 w-4" />
+//               <span>Settings</span>
+//               <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+//             </DropdownMenuItem>
+//             <DropdownMenuItem>
+//               <Keyboard className="mr-2 h-4 w-4" />
+//               <span>Keyboard shortcuts</span>
+//               <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+//             </DropdownMenuItem>
+//           </DropdownMenuGroup>
+//           <DropdownMenuSeparator />
+//           <DropdownMenuGroup>
+//             <DropdownMenuItem>
+//               <Users className="mr-2 h-4 w-4" />
+//               <span>Team</span>
+//             </DropdownMenuItem>
+//             <DropdownMenuSub>
+//               <DropdownMenuSubTrigger>
+//                 <UserPlus className="mr-2 h-4 w-4" />
+//                 <span>Invite users</span>
+//               </DropdownMenuSubTrigger>
+//               <DropdownMenuPortal>
+//                 <DropdownMenuSubContent>
+//                   <DropdownMenuItem>
+//                     <Mail className="mr-2 h-4 w-4" />
+//                     <span>Email</span>
+//                   </DropdownMenuItem>
+//                   <DropdownMenuItem>
+//                     <MessageSquare className="mr-2 h-4 w-4" />
+//                     <span>Message</span>
+//                   </DropdownMenuItem>
+//                   <DropdownMenuSeparator />
+//                   <DropdownMenuItem>
+//                     <PlusCircle className="mr-2 h-4 w-4" />
+//                     <span>More...</span>
+//                   </DropdownMenuItem>
+//                 </DropdownMenuSubContent>
+//               </DropdownMenuPortal>
+//             </DropdownMenuSub>
+//             <DropdownMenuItem>
+//               <Plus className="mr-2 h-4 w-4" />
+//               <span>New Team</span>
+//               <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+//             </DropdownMenuItem>
+//           </DropdownMenuGroup>
+//           <DropdownMenuSeparator />
+//           <DropdownMenuItem>
+//             <Github className="mr-2 h-4 w-4" />
+//             <span>GitHub</span>
+//           </DropdownMenuItem>
+//           <DropdownMenuItem>
+//             <LifeBuoy className="mr-2 h-4 w-4" />
+//             <span>Support</span>
+//           </DropdownMenuItem>
+//           <DropdownMenuItem disabled>
+//             <Cloud className="mr-2 h-4 w-4" />
+//             <span>API</span>
+//           </DropdownMenuItem>
+//           <DropdownMenuSeparator />
+//           <DropdownMenuItem>
+//             <LogOut className="mr-2 h-4 w-4" />
+//             <span>Log out</span>
+//             <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+//           </DropdownMenuItem>
+//         </DropdownMenuContent>
+//       </DropdownMenu>
+//     )
+//   }
