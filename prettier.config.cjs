990f84ff16144a889f17ce2d1a8d40ba27487b64
iAverages <31937542+iAverages@users.noreply.github.com>
2023-09-11 21:10:50 +0100

    Fix ci, format yaml properly

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,6 +3,7 @@ const config = {
     plugins: [require.resolve("prettier-plugin-tailwindcss")],
     printWidth: 120,
     tabWidth: 4,
+    overrides: [{ files: ["*.yaml", "*.yml"], options: { tabWidth: 2 } }],
 };
 
 module.exports = config;
